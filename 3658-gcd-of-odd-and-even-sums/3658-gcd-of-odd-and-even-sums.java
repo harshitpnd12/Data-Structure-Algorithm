cf344a9264ef0bb6464f906095f934f3300eabc2
Harshit Pandey <121434991+harshitpnd12@users.noreply.github.com>
2026-07-15 07:55:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (96.78%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,23 +1,29 @@
+// class Solution {
+//     public int gcdOfOddEvenSums(int n) {
+//         int oddsum=0;
+//         int evensum=0;
+//         int curreven=2;
+//         int currodd=1;
+//         for(int i = 1; i <= n; i++){
+//             evensum+=curreven;
+//             curreven+=2;
+//             oddsum+=currodd;
+//             currodd+=2;
+//         }
+//         return gcd(evensum, oddsum);
+//     }
+//     private int gcd(int a, int b){
+//         while(b != 0){
+//             int temp = b;
+//             b = a % b;
+//             a = temp;
+//         }
+//         return a;
+//     }
+// }
+
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int oddsum=0;
-        int evensum=0;
-        int curreven=2;
-        int currodd=1;
-        for(int i = 1; i <= n; i++){
-            evensum+=curreven;
-            curreven+=2;
-            oddsum+=currodd;
-            currodd+=2;
-        }
-        return gcd(evensum, oddsum);
-    }
-    private int gcd(int a, int b){
-        while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        return n;
     }
 }
