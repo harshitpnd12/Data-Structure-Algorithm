8e77b15b6aa611011add567c849b4c97cf34e9fa
Harshit Pandey <121434991+harshitpnd12@users.noreply.github.com>
2026-09-11 09:32:45 +0530

    Time: 7 ms (34.93%), Space: 46 MB (72.67%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,31 +1,19 @@
 class Solution {
-    public int totalNumbers(int[] digits) {
-         Set<Integer> set = new HashSet<>();
-
-        int n = digits.length;
-
-        for (int i = 0; i < n; i++) {
-            // First digit cannot be 0
-            if (digits[i] == 0) continue;
-
-            for (int j = 0; j < n; j++) {
-                if (j == i) continue;
-
-                for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) continue;
-
-                    // Last digit must be even
-                    if (digits[k] % 2 != 0) continue;
-
-                    int num = digits[i] * 100
-                            + digits[j] * 10
-                            + digits[k];
-
+    public int totalNumbers(int[] arr) {
+        Set<Integer> set=new HashSet<>();
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if (arr[i] == 0) continue;
+            for(int j=0;j<n;j++){
+                if(i==j) continue;
+                for(int k=0;k<n;k++){
+                    if(i==k || k==j) continue;
+                    if (arr[k] % 2 != 0) continue;
+                    int num=arr[i]*100+arr[j]*10+arr[k];
                     set.add(num);
                 }
             }
         }
-
         return set.size();
     }
 }
